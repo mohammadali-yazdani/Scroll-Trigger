5835ea4230e33bebde8d4a03dce4161a2d1d493d
Mohammad Ali Yazdani <mohammadaliyazdani21@gmail.com>
2024-10-13 10:13:24 +0300

    Feat: Register UI thing modules in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,42 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@hypernym/nuxt-gsap"],
+
+  modules: [
+    "@hypernym/nuxt-gsap",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+  ],
+
+  gsap: {
+    composables: true,
+    provide: false,
+    extraPlugins: {
+      scrollTrigger: true,
+    },
+  },
+
+  tailwindcss: {
+    exposeConfig: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
 });
